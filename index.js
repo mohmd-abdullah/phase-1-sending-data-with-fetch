5de24d59956cd30caf6950b86cd9037c1c588bc4
Mohammad <mo7amed.2197@gmail.com>
2023-12-31 18:44:13 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/jason",
+            "Accept": "application/jason",
+        },
+        body: JSON.stringify({
+            userName: userName,
+            email: userEmail
+        }),
+    })
+        .then(res => console.log(res.json()))
+        .then(data => console.log(data))
+        .catch(error => {
+            alert("Bad things!");
+            console.log(error);
+        })
+};
+
+submitData("MHD", "mhd@gmail.com");
